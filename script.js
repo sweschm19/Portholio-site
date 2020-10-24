0cc3438cfe839b23d90935146d5e5a3dde94cd73
A_ShChe <65690436+sweschm19@users.noreply.github.com>
2020-10-24 17:05:51 +0900

    ポートフォリオを更新しました。

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("red-circle").onclick = function(){
+    document.getElementById("red-circle").style.display = "none";
+}
+document.getElementById("green-circle").onclick = function(){
+    document.getElementById("green-circle").style.display = "none";
+}
+document.getElementById("blue-circle").onclick = function(){
+    document.getElementById("blue-circle").style.display = "none";
+}
